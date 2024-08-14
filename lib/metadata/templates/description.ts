b8ecb30812ebc50880f87351ca68f7dfe6e2fa71
keen-finance <112765172+koifarm-office@users.noreply.github.com>
2024-08-14 16:29:27 +0800

    Update description.ts

--- lib/metadata/templates/description.ts
+++ lib/metadata/templates/description.ts
@@ -32,7 +32,7 @@ const TEMPLATE_MAP: Record<Route['pathname'], string> = {
   '/account/custom-abi': DEFAULT_TEMPLATE,
   '/account/tag-address': DEFAULT_TEMPLATE,
   '/account/verified-addresses': DEFAULT_TEMPLATE,
-  '/public-tags/submit': 'Propose a new public tag for your address, contract or set of contracts for your dApp. Our team will review and approve your submission. Public tags are incredible tool which helps users identify contracts and addresses.',
+  '/public-tags/submit': 'Propose a new public tag for your address',
   '/withdrawals': DEFAULT_TEMPLATE,
   '/visualize/sol2uml': DEFAULT_TEMPLATE,
   '/csv-export': DEFAULT_TEMPLATE,
